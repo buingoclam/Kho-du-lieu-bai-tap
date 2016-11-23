e67ea56660140022f3197562dedcd0222adfc03f
buingoclam <buingoclam47@gmail.com>
2016-11-23 20:28:24 +0700

    Nhóm Lâm và Châu
    có gì sai sót thầy hướng dẫn thầy cho bọn em

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace shoot
+{
+    partial class MoleShooter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerGameLoop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerGameLoop
+            // 
+            this.timerGameLoop.Tick += new System.EventHandler(this.timerGameLoop_Tick);
+            // 
+            // MoleShooter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::shoot.Properties.Resources.tải_xuống1;
+            this.ClientSize = new System.Drawing.Size(655, 328);
+            this.Name = "MoleShooter";
+            this.Text = "Mole Shooter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerGameLoop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿#define My_Debug
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace shoot
+{
+    public partial class MoleShooter : Form
+    {
+        int _cursX = 0;
+        int _cursY = 0;
+
+        public MoleShooter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+
+            TextFormatFlags flags = TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+            Font _font = new System.Drawing.Font("Stencil", 12, FontStyle.Regular);
+            TextRenderer.DrawText(dc, "X=" + _cursX.ToString() + ":" + "Y=" + _cursY.ToString(), _font,
+                new Rectangle(0, 0, 120, 20), SystemColors.ControlText, flags);
+            base.OnPaint(e);
+        }
+
+        private void MoleShooter_MouseMove(object sender, MouseEventArgs e)
+        {
+            _cursX = e.X;
+            _cursY = e.Y;
+
+            this.Refresh();
+
+        }
+
+      
+        private void timerGameLoop_Tick(object sender, EventArgs e)
+        {
+
+        }
+       
+
+       
+    }
+
+}
+    
+
